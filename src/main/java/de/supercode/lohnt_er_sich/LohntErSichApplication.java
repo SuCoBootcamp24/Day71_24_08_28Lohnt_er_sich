4e3f7c9319bae7684cbb939cbd50fcd86bee9c93
René Behrens <rene.behrens@aol.com>
2024-08-28 13:48:37 +0200

    init commit

---
+++ src/main/java/de/supercode/lohnt_er_sich/LohntErSichApplication.java
@@ -0,0 +1,13 @@
+package de.supercode.lohnt_er_sich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LohntErSichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LohntErSichApplication.class, args);
+    }
+
+}
